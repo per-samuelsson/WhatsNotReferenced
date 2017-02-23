a874f3273c7f29dc9ec76644d5fce821c06155d4
Per Samuelsson <per@starcounter.com>
2017-02-23 09:10:35 +0100

    Included support to detect columns not referenced.

--- src/WhatsNotReferenced/Program.cs
+++ src/WhatsNotReferenced/Program.cs
@@ -4,12 +4,19 @@ using Starcounter;
 using Starcounter.Metadata;
 using System.Collections.Generic;
 using System.Linq;
+using Starcounter.Internal.Metadata;
 
 namespace WhatsNotReferenced
 {
     public class DatabaseClass
     {
         public string FullName { get; set; }
+    }
+
+    public class DatabaseFieldOrProperty
+    {
+        public string ClassFullName { get; set; }
+        public string Name { get; set; }
     }
 
     public enum ClassInlusion
@@ -28,18 +35,44 @@ namespace WhatsNotReferenced
         /// </summary>
         public IEnumerable<DatabaseClass> Classes { get; private set; }
 
+        /// <summary>
+        /// All database fields/properties not referenced.
+        /// </summary>
+        public IEnumerable<DatabaseFieldOrProperty> FieldsAndProperties { get; private set; }
+
         public static Assets Detect(ClassInlusion inclusion = ClassInlusion.Default)
         {
             var result = new Assets();
 
             var classes = Db.SQL<RawView>("SELECT v FROM RawView v");
-            var notReferenced = classes.Where(c =>
+
+            // Filter on inclusion
+            // TODO:
+
+            var classesNotReferenced = classes.Where(c =>
             {
-                var clr = Db.SQL<ClrClass>("SELECT clr FROM ClrClass clr WHERE clr.UniqueIdentifier = ?", c.UniqueIdentifier).SingleOrDefault();
+                // var clr = Db.SQL<ClrClass>("SELECT clr FROM ClrClass clr WHERE clr.UniqueIdentifier = ?", c.UniqueIdentifier).SingleOrDefault();
+                var clr = Db.SQL<ClrClass>("SELECT clr FROM ClrClass clr WHERE clr.Mapper = ?", c).SingleOrDefault();
                 return clr == null;
             });
 
-            result.Classes = notReferenced.Select(c => new DatabaseClass() { FullName = c.FullName });
+            result.Classes = classesNotReferenced.Select(c => new DatabaseClass() { FullName = c.FullName });
+
+            // For columns:
+            // Get each RawColumn that is not referenced by any MappedProperty.Column
+            //
+            // If the column is part of a dropped table, it's not interesting: it's aready
+            // part of that set.
+
+            var columns = Db.SQL<RawColumn>("SELECT c FROM RawColumn c WHERE c.Inherited = ?", false);
+            var columnsNotReferenced = columns.Where(c =>
+            {
+                var mapped = Db.SQL<MappedProperty>("SELECT m FROM MappedProperty m WHERE m.Column = ?", c).SingleOrDefault();
+                return mapped == null;
+            });
+
+            result.FieldsAndProperties = columnsNotReferenced.Select(c => new DatabaseFieldOrProperty() { ClassFullName = c.Table.FullName, Name = c.Name });
+
             return result;
         }
     }
@@ -53,7 +86,15 @@ namespace WhatsNotReferenced
             Handle.GET("/whatsnotreferenced", () =>
             {
                 var assets = Assets.Detect();
-                return string.Join(Environment.NewLine, assets.Classes.Select(c => c.FullName));
+                var classes = string.Join(Environment.NewLine, assets.Classes.Select(c => c.FullName));
+                var fieldsAndProperties = string.Join(Environment.NewLine, assets.FieldsAndProperties.Select(c => c.ClassFullName + "." + c.Name));
+
+                var result = "Tables:";
+                result += Environment.NewLine + classes;
+                result += Environment.NewLine + Environment.NewLine + "Columns:";
+                result += Environment.NewLine + fieldsAndProperties;
+
+                return result;
             });
         }
     }
