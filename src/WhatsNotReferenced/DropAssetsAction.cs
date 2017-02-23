dc33306af4988153d402fe028cddda642ea8e56b
Per Samuelsson <per@starcounter.com>
2017-02-23 11:00:18 +0100

    New DropAssetsAction type, supporting translating a given set of unreferenced assets to corresponding SQL statements.

---
+++ src/WhatsNotReferenced/DropAssetsAction.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace WhatsNotReferenced
+{
+    public class DropAssetsAction
+    {
+        readonly UnreferencedAssets assets;
+
+        public DropAssetsAction(UnreferencedAssets unreferenced)
+        {
+            assets = unreferenced;
+        }
+
+        public void Execute()
+        {
+            foreach (var table in assets.Tables)
+            {
+                DropTable(table);
+            }
+
+            foreach (var column in assets.Columns)
+            {
+                DropColumn(column);
+            }
+        }
+
+        static void DropTable(UnreferencedTable table)
+        {
+            Console.WriteLine($"DROP TABLE {table.FullName}");
+        }
+
+        static void DropColumn(UnreferencedColumn column)
+        {
+            Console.WriteLine($"ALTER TABLE {column.TableName} DROP COLUMN {column.Name}");
+        }
+    }
+}
